96afc5ce85f44bb8ded88a648f66438b1bf30a07
Elian Batista <elianbatista2000@gmail.com>
2021-04-20 19:06:22 -0300

    welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -29,7 +29,7 @@ export function Welcome() {
         <Text style={styles.buttonText}> 
           >
         </Text>
-    </TouchableOpacity>
+    </TouchableOpacity> 
     </SafeAreaView> 
   )
 }
