e66fe67dfcf13dedf82bd6da6f8ff5e70ed4ff19
Elian Batista <elianbatista2000@gmail.com>
2021-04-20 19:05:57 -0300

    welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native'
-import { Button } from '../components/Button'
+import { 
+  SafeAreaView, 
+  Text, 
+  Image, 
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
 
@@ -17,9 +22,14 @@ export function Welcome() {
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
-      <Button
-        title="Mostrar Imagem"
-      ></Button>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}> 
+          >
+        </Text>
+    </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -47,4 +57,16 @@ const styles = StyleSheet.create({
     width: 292,
     height: 284
   },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonText: {
+    color: colors.white
+  }
 })
