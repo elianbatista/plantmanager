1eb2cba981bc0f2c7d3eeee5a8dd571582fd9798
Elian Batista <elianbatista2000@gmail.com>
2021-04-21 19:51:54 -0300

    Confirmation page

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import { Button } from '../components/Button'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Confirmation() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>
+          
+        </Text>
+        <Text style={styles.title}>
+          Agora vamos começar a cuidar das suas 
+          plantinhas com muito amor.
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <Button />
+      </View>
+    </SafeAreaView>
+  ) 
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  content: {
+
+  },
+  emoji: {
+
+  },
+  title: {
+
+  },
+  footer: {
+    
+  }
+})
