fbc3afe2ed16f190ccd44b9bfa104e25ceb14bbe
Elian Batista <elianbatista2000@gmail.com>
2021-04-21 20:28:25 -0300

    welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -12,8 +12,15 @@ import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/core'
 
 export function Welcome() {
+  const navigation = useNavigation()
+
+  function handleStart() {
+    navigation.navigate('UserIdentification')
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.wrapper}>
@@ -34,6 +41,7 @@ export function Welcome() {
         <TouchableOpacity
           style={styles.button}
           activeOpacity={0.7}
+          onPress={handleStart}
         >
           <Feather 
             name="chevron-right" 
