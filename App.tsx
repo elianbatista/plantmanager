d9279a39bff492abedb51f39f613c0ac27044ebf
Elian Batista <elianbatista2000@gmail.com>
2021-04-21 20:14:33 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading'
-import { Welcome } from './src/pages/Welcome';
-import { UserIdentification } from './src/pages/UserIdentification'
+import Routes from './src/routes';
 import {
   useFonts,
   Jost_400Regular,
@@ -18,6 +17,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <UserIdentification />
+    <Routes />
   )
 }
