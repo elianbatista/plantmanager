4bbaf94797a7025bfe199ddd58f3479763b03fc6
Elian Batista <elianbatista2000@gmail.com>
2021-04-21 20:28:14 -0300

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   TouchableOpacity,
+  TouchableOpacityProps,
   Text,
   StyleSheet
 } from 'react-native'
@@ -8,11 +9,18 @@ import {
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
-export function Button(){
+interface ButtonProps extends TouchableOpacityProps {
+  title: string
+}
+
+export function Button({ title, ...rest }: ButtonProps){
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity 
+      style={styles.container}
+      {...rest}
+    >
       <Text style={styles.text}>
-        Confirmar
+        { title }
       </Text>
     </TouchableOpacity>
   )
